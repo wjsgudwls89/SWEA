d4d78063036e9366a4b68983fc5a87e087b16664
wjsgudwls89 <33246432+wjsgudwls89@users.noreply.github.com>
2019-08-10 16:17:26 +0900

    SWEA3459 - 승자예측하기 (미완)
    꼭 다시한번풀어보기

---
+++ SWEA3459.cpp
@@ -0,0 +1,42 @@
+////
+//// Created by 전형진 on 2019-07-31.
+////
+//#include <stdio.h>
+//#include <algorithm>
+//
+//using namespace std;
+////int test;
+//int N;
+//int x = 1;
+//int flag = true;
+//int A(int x){
+//    return 2 * x;
+//}
+//int B(int x){
+//    return (2 * x) +1;
+//}
+//int main() {
+//
+//    //scanf("%d",&test);
+//    //for (int i = 0; i < test; ++i) {
+//        scanf("%d",&N);
+//        int i = 0;
+//        int a ;
+//        int b ;
+//        while(1){
+//            i++;
+//            a = A(x);
+//            b = B(x);
+//
+//            if( a > N  && b > N){
+//                break;
+//            }
+//            else{
+//                x = max(a,b);
+//                if(x>N)
+//                    x = min(a,b);
+//            }
+//        }
+//        printf("%d", i);
+//    //}
+//}
